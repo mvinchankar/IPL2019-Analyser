b6e9b654f494f9a911039d21738712c764a7831d
mvinchankar <mvinchankar18@gmail.com>
2019-12-14 11:22:42 +0530

    ADDED : Test case to count the number of Records in IPLFactSheetMostRuns file

--- src/test/java/com/iplanalyser/IPLAnalyserTest.java
+++ src/test/java/com/iplanalyser/IPLAnalyserTest.java
@@ -7,4 +7,14 @@ import java.util.Map;
 
 public class IPLAnalyserTest {
 
+    private static final String IPL_2019_FACTSHEET_MOST_RUNS_CSV_FILE_PATH
+            ="src/test/resources/IPL2019FactsheetMostRuns.csv";
+
+    @Test
+    public void givenIndiaCensusData_ShouldReturnCorrectRecords() {
+        IPLAnalyser iplAnalyser = new IPLAnalyser();
+        Map<String, IPLMostRunsCSV> map = iplAnalyser.loadIPLData(IPL_2019_FACTSHEET_MOST_RUNS_CSV_FILE_PATH);
+        Assert.assertEquals(100, map.size());
+    }
+
 }
