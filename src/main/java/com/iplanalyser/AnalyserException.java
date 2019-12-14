7dfa0547d04db52a862a968bffe51c09d951861d
mvinchankar <mvinchankar18@gmail.com>
2019-12-14 11:41:41 +0530

    ADDED : Code to catch Custom Exception when incorrect file path given.

--- src/main/java/com/iplanalyser/AnalyserException.java
+++ src/main/java/com/iplanalyser/AnalyserException.java
@@ -1,4 +1,15 @@
 package com.iplanalyser;
 
-public class AnalyserException {
+public class AnalyserException extends Exception {
+
+    enum ExceptionType {
+        NO_SUCH_FILE,CENSUS_FILE_PROBLEM
+    }
+
+    ExceptionType type;
+
+    public AnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
 }
--- src/main/java/com/iplanalyser/IPLAnalyser.java
+++ src/main/java/com/iplanalyser/IPLAnalyser.java
@@ -7,6 +7,7 @@ import com.csvbuilder.ICSBuilder;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -15,18 +16,19 @@ import java.util.stream.StreamSupport;
 
 public class IPLAnalyser {
 
-    public Map<String,IPLMostRunsCSV> loadIPLData(String filePath) {
+    public Map<String, IPLMostRunsCSV> loadIPLData(String filePath) throws AnalyserException {
         Map<String, IPLMostRunsCSV> iplMostRunsCSVHashMapMap = new HashMap<>();
         try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
             ICSBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
             Iterator<IPLMostRunsCSV> censusCSVIterator = csvBuilder.getCSVFileIterator(reader, IPLMostRunsCSV.class);
             Iterable<IPLMostRunsCSV> censusDAOIterable = () -> censusCSVIterator;
             StreamSupport.stream(censusDAOIterable.spliterator(), false)
-                        .forEach(censusCSV -> iplMostRunsCSVHashMapMap.put(censusCSV.playerName,new IPLMostRunsCSV(censusCSV)));
+                    .forEach(censusCSV -> iplMostRunsCSVHashMapMap.put(censusCSV.playerName, new IPLMostRunsCSV(censusCSV)));
             return iplMostRunsCSVHashMapMap;
-        } catch (IOException | CSVBuilderException e){
-            e.printStackTrace();
+        } catch (NoSuchFileException e) {
+            throw new AnalyserException(e.getMessage(), AnalyserException.ExceptionType.NO_SUCH_FILE);
+        } catch (IOException | CSVBuilderException e) {
+            throw new AnalyserException(e.getMessage(), AnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
-        return null;
     }
 }
